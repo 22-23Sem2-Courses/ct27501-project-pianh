a3f88b023772a47a85a3b3ff3dec8d33ab811e99
Duy Anh <aduy644@gmail.com>
2023-03-10 21:07:28 +0700

    add index.php file khachhang backend

--- public/backend/khachhang/index.php
+++ public/backend/khachhang/index.php
@@ -1,11 +1,21 @@
+<?php
+
+    require_once '../../../bootstrap.php';
+
+    use DientuCT\Project\Khachhang;
+    $khachhang = new Khachhang($PDO);
+    $khachhang = $khachhang->all();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <?php include_once __DIR__ . '/../layouts/meta.php'; ?>
 
     <title>Danh sách các khách hàng</title>
-    
+
+
     <?php include_once __DIR__ . '/../layouts/styles.php'?>
+    
 </head>
 <body>
     <?php include_once __DIR__ . '/../layouts/partials/header.php' ?>
@@ -15,52 +25,25 @@
             <?php include_once __DIR__ . '/../layouts/partials/sidebar.php' ?>
 
 
-            <div class="col-md-10">
-                </br>
-                <h1 class="text-center">Danh sách các khách hàng hiện có</h1>
+            <div class="col-md-10 justify-content-center">
                 <?php
-                    // Hiển thị tất cả lỗi trong PHP
-                    // Chỉ nên hiển thị lỗi khi đang trong môi trường Phát triển (Development)
-                    // Không nên hiển thị lỗi trên môi trường Triển khai (Production)
                     ini_set('display_errors', 1);
                     ini_set('display_startup_errors', 1);
                     error_reporting(E_ALL);
-                    //1. Mở kết nối đến database
-                    include_once __DIR__ . '/../../dbconnect.php';
-                    //2. Chuan bi cau lenh
-                    $sql = "SELECT * FROM khachhang;";
-
-                    //3. Thuc thi
-                    $result = mysqli_query($conn, $sql);
-
-                    //4. Phân tách thành mảng array
-                    $data = [];
-                    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                        $data[] = array (
-                            'kh_tendangnhap' => $row['kh_tendangnhap'],
-                            'kh_ten' => $row['kh_ten'],
-                            'kh_gioitinh' => $row['kh_gioitinh'],
-                            'kh_dienthoai' => $row['kh_dienthoai'],
-                            'kh_cmnd' => $row['kh_cmnd'],
-                            'kh_quantri' => $row['kh_quantri'],
-                            'kh_quanly' => $row['kh_quanly']
-                           
-                        );
-                    }
-
-                    //  var_dump($data);
-                    //  die;
+                    
                 ?>
-                
-                <a href="create.php" class="btn btn-primary">Thêm mới</a>
+                </br>
+                <h2 class="text-center">Danh sách các khách hàng hiện có</h2>
+                <a href="create.php" class="btn btn-primary">
+                    <i class="fa fa-plus"></i>Thêm mới</a>
                 </br>
                 </br>
-                    <table class="table table-bordered table-hover">
-                        <thead class="table-info">
+                <table id="tblKhachhang" class="table table-bordered table-hover table-responsive-lg table-striped table-sm">
+                        <thead class="text-center">
                             <tr>
                                 <th>Tên đăng nhập</th>
-                                <th>Tên Thành viên</th>
-                                <th>Giới Tính</th>
+                                <th>Tên khách hàng</th>
+                                <th>Giới tính</th>
                                 <th>Số điện thoại</th>
                                 <th>CMND</th>
                                 <th>Quản lý</th>
@@ -68,44 +51,57 @@
                                 <th>Hành động</th>
                             </tr>
                         </thead>
-                    <?php foreach($data as $kh): ?>
-                        <tr>
-                            <td><?= $kh['kh_tendangnhap']  ?>  </td>
-                            <td><?php echo $kh['kh_ten']  ?></td>
-                            <td><?php 
-                            if ($kh['kh_gioitinh'] == 1)
-                                echo "Nam";
-                            else 
-                                echo "Nữ";
-                           
-                            ?></td>
-                            <td><?php echo $kh['kh_dienthoai']  ?></td>
-                            <td><?php echo $kh['kh_cmnd']  ?></td>
-                            <td><?php 
-                            if ($kh['kh_quanly'] == 1)
-                                echo "X";
-                            else echo "";
-                            ?></td>
-                            <td><?php 
-                            if ($kh['kh_quantri'] == 1)
-                                echo "X";
-                            else echo "";
-                            ?></td>
-                            <td>
-                                <!-- Nút sửa -->
-                                <a href="edit.php?kh_tendangnhap=<?= $kh['kh_tendangnhap']?>" class="btn btn-warning">Sửa</a>
-                                <!-- Nút xóa -->
-                                <!-- <a href="delete.php?kh_tendangnhap=<?= $kh['kh_tendangnhap']?>" class="btn btn-danger">Xóa</a> -->
-                                <button type="button" class="btn btn-danger btnDelete" data-kh_tendangnhap="<?= $kh['kh_tendangnhap'] ?>">
-                                    Xóa
-                                </button>
-                                </form>
-                            </td>
-
-
-                        </tr>
-                    <?php endforeach; ?>
-                    </table>    
+                        <tbody>
+                            <?php foreach($khachhang as $khachhang): ?>
+                                    <tr>
+                                        <td><?=htmlspecialchars($khachhang->kh_tendangnhap)?></td>
+                                        <td><?=htmlspecialchars($khachhang->kh_ten)?></td>
+                                        <td>
+                                            <?php 
+                                            if (htmlspecialchars($khachhang->kh_gioitinh) == 1)
+                                                echo "Nam";
+                                            else 
+                                                echo "Nữ";
+                                            ?>
+                                        </td>
+                                        <td><?=htmlspecialchars($khachhang->kh_dienthoai)?></td>
+                                        <td><?=htmlspecialchars($khachhang->kh_cmnd)?></td>
+                                        <td class="text-center">
+                                            <?php 
+                                                if (htmlspecialchars($khachhang->kh_quanly) == 1)
+                                                    echo "X";
+                                                else echo "";
+                                            ?>
+                                        </td>
+                                        <td class="text-center">
+                                            <?php 
+                                                if (htmlspecialchars($khachhang->kh_quantri) == 1)
+                                                    echo "X";
+                                                else echo "";
+                                            ?>
+                                        </td>                                    
+                                        </td>
+                                        <td>
+                                            <a href="<?=BASE_URL_PATH . 'backend/khachhang/edit.php?id=' . $khachhang->getTendangnhap()?>"
+                                                class="btn btn-xs btn-warning">
+                                                <i alt="Edit" class="fa fa-pencil"></i> Sửa</a>
+                                            <!-- <form class="delete" action="<?=BASE_URL_PATH . 'backend/khachhang/delete.php'?>"
+                                                    method="POST" style="display: inline;">
+                                                <input type="hidden" name="id"
+                                                    value="<?=$khachhang->getTendangnhap()?>">
+                                                <button type="submit" class="btn btn-xs btn-danger btnDelete"
+                                                    name="delete-khachhang">
+                                                    <i alt="Delete" class="fa fa-trash">Xóa</i></button>
+                                            </form> -->
+                        
+                                            <button type="button" class="btn btn-danger btnDelete" data-kh_tendangnhap="<?= $khachhang->getTendangnhap() ?>">
+                                                <i alt="Delete" class="fa fa-trash"></i> Xóa
+                                            </button>
+                                        </td>
+                                    </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                </table>    
 
             </div>
         
@@ -117,10 +113,53 @@
     <?php include_once __DIR__ . '/../layouts/partials/footer.php' ?>
     <?php include_once __DIR__ . '/../layouts/scripts.php' ?>
        <!-- SweetAlert -->
-       <script>
-   
-   
+    <script>
+   $(document).ready(function() {
+            // Yêu cầu DataTable quản lý datatable #tblKhachhang
 
+            $('#tblKhachhang').DataTable({
+                dom: 'Blfrtip',
+                "bProcessing": true,
+                "bAutoWidth": false,
+                "responsive": true,
+                "buttons": [
+                    'copy', 'excel', 'csv', 'pdf'
+                ]
+            });
+
+            // Cảnh báo khi xóa
+            // 1. Đăng ký sự kiện click cho các phần tử (element) đang áp dụng class .btnDelete
+            $('.btnDelete').click(function() {
+                // Click hanlder
+                // 2. Sử dụng thư viện SweetAlert để hiện cảnh báo khi bấm nút xóa
+                swal({
+                        title: "Bạn có chắc chắn muốn xóa?",
+                        text: "Một khi đã xóa, không thể phục hồi....",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) { // Nếu đồng ý xóa
+
+                            // 3. Lấy giá trị của thuộc tính (custom attribute HTML) 'dh_ma'
+                            // var dh_ma = $(this).attr('data-dh_ma');
+                            var kh_tendangnhap = $(this).data('kh_tendangnhap');
+                            var url = "delete.php?kh_tendangnhap=" + kh_tendangnhap;
+
+                            // Điều hướng qua trang xóa với REQUEST GET, có tham số km_ma=...
+                            location.href = url;
+                        } else { // Nếu không đồng ý xóa
+                            swal("Cẩn thận hơn nhé!");
+                        }
+                    });
+
+            });
+        });
+      
+  
+   
+    
     </script>
 </body>
 </html>
