de10a36465120dd754eb4f61e19de37a0d28409e
Duy Anh <aduy644@gmail.com>
2023-03-15 20:27:52 +0700

    edit code bootstrap.php file

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,25 @@
 <?php
 
-require_once 'libraries/Psr4AutoloaderClass.php';
+define('BASE_URL_PATH', '/');
+require_once __DIR__ . '/src/functions.php';
+require_once __DIR__ . '/libraries/Psr4AutoloaderClass.php';
 
 $loader = new Psr4AutoloaderClass;
-
 $loader->register();
 
-$loader->addNamespace(
-	'App\CT275',
-	__DIR__ . '/src'
-);
+// Các lớp có không gian tên bắt đầu với DientuCT\Project nằm ở src
+$loader->addNamespace('DientuCT\Project', __DIR__ .'/src');
+
+try {
+    $PDO = (new DientuCT\Project\PDOFactory)->create([
+    'dbhost' => 'localhost',
+    'dbname' => 'dientuct',
+    'dbuser' => 'root',
+    'dbpass' => ''
+    ]);
+    } catch (Exception $ex) {
+        echo 'Không thể kết nối đến MySQL, kiểm tra lại username/password đến MySQL.<br>';
+    exit("<pre>${ex}</pre>");
+    }
+
+//require_once __DIR__ . '/src/functions.php';
