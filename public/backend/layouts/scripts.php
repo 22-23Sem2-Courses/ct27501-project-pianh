effc1c547d5f119b9b398e70f7852be0d7888765
Duy Anh <aduy644@gmail.com>
2023-03-15 20:34:16 +0700

    update code meta, scripts, styles

---
+++ public/backend/layouts/scripts.php
@@ -0,0 +1,24 @@
+<!-- jQuery JS -->
+<script src="../../assets/backend/js/jquery.min.js"></script>
+
+
+<!-- Bootstrap JS -->
+<script src="../../assets/backend/js/bootstrap.min.js"></script>
+
+<!-- Wow js -->
+<script src="../../assets/backend/js/wow.min.js"></script>
+
+<!-- SweetAlert JS-->
+<script src="../../assets/backend/js/sweetalert.js"></script>
+<script src="../../assets/backend/js/sweetalert.min.js"></script>
+
+
+<!-- Chart JS-->
+<script src="../../assets/backend/js/chart.min.js"></script>
+
+<!-- DataTable JS -->
+<script src="../../assets/backend/js/datatables.min.js"></script>
+
+<script src="../../assets/backend/js/buttons.bootstrap4.min.js"></script>
+<script src="../../assets/backend/js/pdfmake.min.js"></script>
+<script src="../../assets/backend/js/vfs_fonts.js"></script>
--- public/backend/layouts/styles.php
+++ public/backend/layouts/styles.php
@@ -1,14 +1,26 @@
 <!-- Bootstrap CSS -->
-<link rel="stylesheet" href="/dientucantho.vn/libraries/bootstrap/css/bootstrap.min.css" type="text/css" />
+<link rel="stylesheet" href="../../assets/backend/css/bootstrap.min.css" type="text/css" />
+
 
 <!-- Font awesome -->
-<link rel="stylesheet" href="/libraries/font-awesome/css/all.min.css" type="text/css" />
+<link rel="stylesheet" href="../../assets/backend/css/font-awesome.min.css" type="text/css" />
+
+
 
 <!-- Paper CSS -->
-<!-- <link rel="stylesheet" href="/libraries/paper-css/paper.css" type="text/css" /> -->
+<!-- <link rel="stylesheet" href="../../assets/backend/css/paper.css" type="text/css" /> -->
+
+
+<!-- Datatables css -->
+<link href="../../assets/backend/css/datatables.min.css" rel="stylesheet"/>
+
+
+<!-- Animate css -->
+<link href="../../assets/backend/css/animate.css" rel="stylesheet"/>
 
 <!-- Custom css -->
-<link rel="stylesheet" href="/backend/css/base.css" type="text/css" />
-<link rel="stylesheet" href="/backend/css/responsive.css" type="text/css" />
-<link rel="stylesheet" href="/backend/css/styles.css" type="text/css" />
+<link rel="stylesheet" href="../../assets/backend/css/base.css" type="text/css" />
+<link rel="stylesheet" href="../../assets/backend/css/responsive.css" type="text/css" />
+<link rel="stylesheet" href="../../assets/backend/css/styles.css" type="text/css" />
+
 
