b7a8bc6d638dbfc2d380e66c9cf609b15ba2e503
Duy Anh <aduy644@gmail.com>
2023-03-15 20:29:44 +0700

    delete partials file

--- public/backend/layouts/partials/show_error.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$error_message ??= 'Đã có lỗi xảy ra';
-$error = "<p class=\"error\">$error_message";
-$error .= isset($reason) ? " vì:<br>$reason</p>" : "</p>";
-$error .= isset($query) ? "<p>Câu truy vấn là: {$query}</p>" : '';
-echo $error;
