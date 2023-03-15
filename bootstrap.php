aec37476a230f9dbd5725d71c65929b9675a86d1
Duy Anh <aduy644@gmail.com>
2023-03-15 20:54:00 +0700

    edit bootstrap file

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,14 @@
 <?php
 
 define('BASE_URL_PATH', '/');
-require_once __DIR__ . '/src/functions.php';
+require_once __DIR__ . '/classes/functions.php';
 require_once __DIR__ . '/libraries/Psr4AutoloaderClass.php';
 
 $loader = new Psr4AutoloaderClass;
 $loader->register();
 
-// Các lớp có không gian tên bắt đầu với DientuCT\Project nằm ở src
-$loader->addNamespace('DientuCT\Project', __DIR__ .'/src');
+// Các lớp có không gian tên bắt đầu với DientuCT\Project nằm ở classes
+$loader->addNamespace('DientuCT\Project', __DIR__ .'/classes');
 
 try {
     $PDO = (new DientuCT\Project\PDOFactory)->create([
@@ -22,4 +22,4 @@ try {
     exit("<pre>${ex}</pre>");
     }
 
-//require_once __DIR__ . '/src/functions.php';
+//require_once __DIR__ . '/classes/functions.php';
