fcf145041d2ac96a51fed0ffffc288aa33e49d1a
Duy Anh <aduy644@gmail.com>
2023-04-05 16:14:30 +0700

    edit file boots json

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,15 @@
 <?php
 
 define('BASE_URL_PATH', '/');
-require_once __DIR__ . '/classes/functions.php';
+require_once __DIR__ . '/src/classes/functions.php';
 require_once __DIR__ . '/libraries/Psr4AutoloaderClass.php';
 
 $loader = new Psr4AutoloaderClass;
 $loader->register();
 
 // Các lớp có không gian tên bắt đầu với DientuCT\Project nằm ở classes
-$loader->addNamespace('DientuCT\Project', __DIR__ .'/classes');
+$loader->addNamespace('DientuCT\Project', __DIR__ .'/src/api');
+$loader->addNamespace('DientuCT\Project', __DIR__ .'/src/classes');
 
 try {
     $PDO = (new DientuCT\Project\PDOFactory)->create([
