28704f9c76c4eaf2b9630552329cf2fd8fc1ba1f
Duy Anh <aduy644@gmail.com>
2023-04-16 18:43:46 +0700

    add about page

---
+++ public/about.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Điện tử Cần Thơ | Mua bán điện thoại, Tablet, Laptop</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/assets/user/css/bootstrap.min.css" type="text/css" />
+    <!-- Font awesome -->
+    <link rel="stylesheet" href="/assets/user/css/font-awesome.min.css" type="text/css" />
+    <!-- Datatables CSS -->
+    <link href="/assets/user/css/datatables.min.css" rel="stylesheet"/>
+    <!-- Animate CSS -->
+    <link href="/assets/user/css/animate.css" rel="stylesheet"/>
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/assets/user/css/base.css" type="text/css" />
+    <link rel="stylesheet" href="/assets/user/css/styles.css" type="text/css" />
+    <link rel="stylesheet" href="/assets/user/css/responsive.css" type="text/css" />
+    
+    
+</head>
+<body>
+    <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);     
+    ?>
+
+    <?php include_once __DIR__ . '../../partials/user/header.php'; ?>
+
+    <div class="body_container">
+        <main role="main " class="">
+            
+            <div class="col-md-12 col-sm-12 main-column">
+                
+                <section class="jumbotron text-center form-search">
+                    <div class="container">
+                        <h1 class="jumbotron-heading">Tìm kiếm chúng tôi trên bản đồ</h1>
+                        <p class="lead text-muted">Chất lượng, uy tín, hàng đầu cả nước.</p>
+                    </div>
+                </section>
+                
+                <!-- Google map -->
+                <div class="row mt-2 mb-5">
+                    <div class="col col-md-12">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.7235485722294!2d105.78061631523369!3d10.039656175103817!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a062a768a8090b%3A0x4756d383949cafbb!2zMTMwIFjDtCBWaeG6v3QgTmdo4buHIFTEqW5oLCBBbiBI4buZaSwgTmluaCBLaeG7gXUsIEPhuqduIFRoxqEsIFZp4buHdCBOYW0!5e0!3m2!1svi!2s!4v1556697525436!5m2!1svi!2s" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+                
+
+
+                
+
+                
+
+                
+
+                
+                
+            </div>
+
+        </main>
+
+        <!-- Footer top -->
+        <div class="footer-top row">
+            <div class="col-md-2">
+                <h3 class="footer__heading">Chăm sóc khách hàng</h3>
+                <div class="footer-list">
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Trung tâm trợ giúp</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Mua hàng và thanh toán Online</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Trung tâm bảo hành chính hãng</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3 class="footer__heading">Thông tin liên hệ</h3>
+                <div class="footer-list">
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Giới thiệu công ty</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Hệ thống cửa hàng</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Chính sách bảo mật</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3 class="footer__heading">Gọi tư vấn & khiếu nại</h3>
+                <div class="footer-list">
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Gọi mua hàng: 085 5100 001 (8h00 - 22h00)</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Hỗ trợ kỹ thuật: 1800 6502 (8h00 - 21h00)</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Hợp tác kinh doanh: 1900 6122</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3 class="footer__heading">Tuyển dụng</h3>
+                <div class="footer-list">
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Thông tin tuyển dụng</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Vị trí ứng tuyển</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Liên hệ nộp CV</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3 class="footer__heading">Thông tin khác</h3>
+                <div class="footer-list">
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Chính sách đổi trả</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Quy chế hoạt động</a>
+                    </div>
+                    <div class="footer-item">
+                        <a href="" class="footer-item__link">Chính sách Bảo hành</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <h3 class="footer__heading">Điện tử Cần Thơ trên ứng dụng</h3>
+                <div class="footer__download">
+                    <img src="/assets/user/imgs/qr_code_app.png" alt="Download QR" class="footer__download-qr">
+                    <div class="footer__download-apps">
+                        <a href="" class="footer__download-apps-link">
+                            <img src="/assets/user/imgs/google_play.png" alt="Google Play" class="footer__download-app-img">
+                        </a>
+                        <a href="" class="footer__download-apps-link">
+                            <img src="/assets/user/imgs/app_store.png" alt="App Store" class="footer__download-app-img">
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+        
+    </div>
+
+    <?php include_once __DIR__ . '../../partials/user/footer.php'; ?>
+    <!-- jQuery JS -->
+    <script src="/assets/user/js/jquery.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="/assets/user/js/bootstrap.min.js"></script>
+    <!-- Wow js -->
+    <script src="/assets/user/js/wow.min.js"></script>
+    <!-- SweetAlert JS-->
+    <script src="/assets/user/js/sweetalert.js"></script>
+    <script src="/assets/user/js/sweetalert.min.js"></script>
+    <!-- DataTable JS -->
+    <script src="/assets/user/js/datatables.min.js"></script>
+    <script src="/assets/user/js/buttons.bootstrap4.min.js"></script>
+    <script src="/assets/user/js/pdfmake.min.js"></script>
+    <script src="/assets/user/js/vfs_fonts.js"></script>
+    <!-- Custom JS -->
+    <script src="/assets/user/js/app.js"></script>
+
+
+</body>
+</html>
